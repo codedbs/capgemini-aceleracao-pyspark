1208b289a77f5475ca7affa0f09e9ad5cf990221
Daniel Santos <dbispods@gmail.com>
2022-04-14 18:34:09 -0300

    updated with functions, but only preview

--- scripts/online-retail.py
+++ scripts/online-retail.py
@@ -5,6 +5,8 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 import pandas as pd
+import pyspark.sql
+
 import numpy as np
 from pyspark.sql.functions import from_unixtime, unix_timestamp,expr
 from pyspark.sql.functions import isnan, when, count, col
@@ -119,6 +121,189 @@ def col_unitprice_tf(df):
 
 ### ---------------------------------------------- ###
 
+# answering some business questions #
+
+def question_1(df):
+	(df
+	.filter((F.col('StockCode').startswith('gift_0001')) & # Only gift cards starts with 'gift_001' #
+			(~F.col('InvoiceNo').startswith('C')) & # Disregarding canceled sales #
+			(F.col('Quantity') > 0)) # only completed sales #
+	.agg({'Quantity' : 'sum'})
+	.show())
+
+def question_2(df):
+	
+	(df
+	.filter((F.col('StockCode').startswith('gift_0001')) & # Only gift cards starts with 'gift_001' #
+			(~F.col('InvoiceNo').startswith('C')) & # Disregarding canceled sales #
+			(F.col('Quantity') > 0)) # only completed sales #
+	.groupBy(F.month('InvoiceDate'))
+	.sum('Quantity')
+	.orderBy('month(InvoiceDate)')
+	.show())
+
+def question_3(df):
+
+	(df
+	.filter((F.col('StockCode') == 'S') & # Only samples #
+			(~F.col('InvoiceNo').startswith('C')) & # Disregarding canceled sales #
+			(F.col('Quantity') > 0))  # only completed sales #
+	.groupBy('StockCode').sum('Quantity')
+	.show())
+	
+def question_4(df):
+
+	(df
+	.filter((~F.col('InvoiceNo').startswith('C')) & # Disregarding canceled sales #
+			(F.col('Quantity') > 0) &  # only completed sales #
+			(F.col('StockCode') != 'PADS')) # Disregarding PADS code #
+	.groupBy('StockCode')
+	.sum('Quantity')
+	.orderBy(F.col('sum(Quantity)').desc())
+	.show())
+
+
+def question_5(df):
+
+	df = (
+		df
+		.filter((~F.col('InvoiceNo').startswith('C')) & # Disregarding canceled sales #
+				(F.col('Quantity') > 0) &  # only completed sales #
+				(F.col('StockCode') != 'PADS')) # Disregarding PADS code #
+		.groupBy('StockCode', F.month('InvoiceDate'))
+		.sum('Quantity')
+		.orderBy(F.col('sum(Quantity)').desc())
+	)
+
+
+	df = df.select('StockCode',
+				F.col('month(InvoiceDate)').alias('month'),
+				F.col('sum(Quantity)').alias('sum_quantity'))
+	
+	df_max_per_month = df.groupBy('month').max('sum_quantity')
+
+	df_max_per_month = df_max_per_month.join(df.alias('b'), 
+								F.col('b.sum_quantity') == F.col('max(sum_quantity)'),
+								"left").select('b.month','StockCode','sum_quantity')
+	
+	df_max_per_month.orderBy('month').show()
+
+
+def question_6(df):
+	
+	(df
+	.filter(F.col('valor_de_venda') > 0 &  # only completed sales #
+	 		(F.col('StockCode') != 'PADS')) # Disregarding PADS code #
+	.groupBy(F.hour('InvoiceDate'))
+	.sum('valor_de_venda')
+	.orderBy(F.col('sum(valor_de_venda)').desc())
+	.show())
+
+
+
+
+def question_7(df):
+	
+	(df
+	.filter(F.col('valor_de_venda') > 0 &   # only completed sales #
+			(F.col('StockCode') != 'PADS')) # Disregarding PADS code #
+	.groupBy(F.month('InvoiceDate'))
+	.sum('valor_de_venda')
+	.orderBy(F.col('sum(valor_de_venda)').desc())
+	.show())
+
+
+
+
+def question_8(df):
+
+	(df
+	.filter((F.col('valor_de_venda') > 0) &  # only completed sales #
+			(F.col('StockCode') != 'PADS')) # Disregarding PADS code #
+	.groupBy(F.month('InvoiceDate'), F.col('StockCode'))
+	.sum('valor_de_venda')
+	.orderBy(F.col('sum(valor_de_venda)').desc())
+	.show(1))
+
+
+
+def question_9(df):
+
+	(df
+	.filter((F.col('valor_de_venda') > 0) & # only completed sales #
+			(F.col('StockCode') != 'PADS')) # Disregarding PADS code #
+	.groupBy('Country')
+	.sum('valor_de_venda')
+	.orderBy(F.col('sum(valor_de_venda)').desc())
+	.show(1))
+
+
+
+def question_10(df):
+		
+	(df
+	.filter((F.col('valor_de_venda') > 0) & # only completed sales #
+			(F.col('StockCode') != 'PADS') & # Disregarding PADS code #
+			(F.col('StockCode') == 'M')) # Only manual sales #
+	.groupBy('Country')
+	.sum('valor_de_venda')
+	.orderBy(F.col('sum(valor_de_venda)').desc())
+	.show(1))
+
+
+
+
+
+def question_11(df):
+
+	maior_valor_de_venda = (df
+				.filter(F.col('StockCode') != 'PADS') # Disregarding PADS code #
+				.agg({'valor_de_venda' : 'max'})
+				.first()[0])
+
+	(df
+	.select('InvoiceNo','valor_de_venda')
+	.filter(F.col('valor_de_venda') == maior_valor_de_venda)
+	.show())
+
+
+def question_12(df):
+
+	maior_n_de_itens = (df
+				.filter(F.col('StockCode') != 'PADS') # Disregarding PADS code #
+				.agg({'Quantity' : 'max'})
+				.first()[0])
+
+	(df
+	.select('InvoiceNo','Quantity')
+	.filter(F.col('Quantity') == maior_n_de_itens)
+	.show())
+
+def question_13(df):
+
+	mais_frequente = (df
+					.filter(
+						(F.col('StockCode') != 'PADS') & # Disregarding PADS code #
+						(F.col('CustomerID').isNotNull()) #  Disregarding empty info from CustomerID #
+						)
+					.groupBy('CustomerID')
+					.count()
+					.orderBy(F.col('count').desc())
+					.first()[1])
+
+	(df
+	.filter(
+		(F.col('StockCode') != 'PADS') & # Disregarding PADS code #
+		(F.col('CustomerID').isNotNull()) #  Disregarding empty info from CustomerID #
+		)
+	.groupBy('CustomerID')
+	.count()
+	.filter(F.col('count') == mais_frequente)
+	.show())
+
+### ---------------------------------------------- ###
+
+# main session #
 
 if __name__ == "__main__":
 	sc = SparkContext()
@@ -142,3 +327,63 @@ if __name__ == "__main__":
                   .load("/home/spark/capgemini-aceleracao-pyspark-old/data/online-retail/online-retail.csv"))
     
     
+### ---------------------------------------------- ###
+
+# calling previous functions #
+
+#df = col_quantity_qa(df)
+#df = col_quantity_tf(df)
+#question_1(df)
+
+#df = col_quantity_qa(df)
+#df = pergunta_2_tr(df)
+#pergunta_2(df)
+
+#df = pergunta_3_qa(df)
+#df = pergunta_3_tr(df)
+#pergunta_3(df)
+
+#df = pergunta_4_qa(df)
+#df = pergunta_4_tr(df)
+#pergunta_4(df)
+
+#df = pergunta_5_qa(df)
+#df = pergunta_5_tr(df)
+#pergunta_5(df)
+
+#df = pergunta_6_qa(df)
+#df = pergunta_6_tr(df)
+#pergunta_6(df)
+
+#df = pergunta_7_qa(df)
+#df = pergunta_7_tr(df)
+#pergunta_7(df)
+
+#df = pergunta_8_qa(df)
+#df = pergunta_8_tr(df)
+#pergunta_8(df)
+
+#df = pergunta_9_qa(df)
+#df = pergunta_9_tr(df)
+#pergunta_9(df)
+
+#df = pergunta_10_qa(df)
+#df = pergunta_10_tr(df)
+#pergunta_10(df)
+
+#df = pergunta_11_qa(df)
+#df = pergunta_11_tr(df)
+#pergunta_11(df)
+
+#df = pergunta_12_qa(df)
+#df = pergunta_12_tr(df)
+#pergunta_12(df)
+
+#pergunta_13_qa(df)
+#pergunta_13(df)
+
+
+
+
+
+
