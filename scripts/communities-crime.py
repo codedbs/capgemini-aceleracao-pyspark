a71e45a6c78035eb8cc17bae3b1b79eac53abe6a
Daniel Santos <dbispods@gmail.com>
2022-04-18 16:59:41 -0300

    added schema definition

--- scripts/communities-crime.py
+++ scripts/communities-crime.py
@@ -10,6 +10,142 @@ from pyspark.sql.functions import from_unixtime, unix_timestamp,expr
 from pyspark.sql.functions import isnan, when, count, col
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType, TimestampType
 
+### defining schema ###
+
+
+schema_communities_crime = StructType([ 
+    StructField('state', IntegerType(), True),
+    StructField('county', IntegerType(), True),
+    StructField('community', IntegerType(), True),
+    StructField('communityname', StringType(), True),
+    StructField('fold', IntegerType(), True),
+    StructField('population', FloatType(), True),
+    StructField('householdsize', FloatType(), True),
+    StructField('racepctblack', FloatType(),  True),
+    StructField('racePctWhite',FloatType(), True),
+    StructField('racePctAsian', FloatType(), True),
+    StructField('racePctHisp', FloatType(), True),
+    StructField('agePct12t21', FloatType(), True),
+    StructField('agePct12t29', FloatType(), True),
+    StructField('agePct16t24', FloatType(), True),
+    StructField('agePct65up', FloatType(), True),
+    StructField('numbUrban', FloatType(), True),
+    StructField('pctUrban', FloatType(), True),
+    StructField('medIncome', FloatType(), True),
+    StructField('pctWWage', FloatType(), True),
+    StructField('pctWFarmSelf', FloatType(), True),
+    StructField('pctWInvInc', FloatType(), True),
+    StructField('pctWSocSec', FloatType(), True),
+    StructField('pctWPubAsst', FloatType(), True),
+    StructField('pctWRetire', FloatType(), True),
+    StructField('medFamInc', FloatType(), True),
+    StructField('perCapInc', FloatType(), True),
+    StructField('whitePerCap', FloatType(), True),
+    StructField('blackPerCap', FloatType(), True),
+    StructField('indianPerCap', FloatType(), True),
+    StructField('AsianPerCap', FloatType(), True),
+    StructField('OtherPerCap', FloatType(), True),
+    StructField('HispPerCap', FloatType(), True),
+    StructField('NumUnderPov', FloatType(), True),
+    StructField('PctPopUnderPov', FloatType(), True),
+    StructField('PctLess9thGrade', FloatType(), True),
+    StructField('PctNotHSGrad', FloatType(), True),
+    StructField('PctBSorMore', FloatType(), True),
+    StructField('PctUnemployed', FloatType(), True),
+    StructField('PctEmploy', FloatType(), True),
+    StructField('PctEmplManu', FloatType(), True),
+    StructField('PctEmplProfServ', FloatType(), True),
+    StructField('PctOccupManu', FloatType(), True),
+    StructField('PctOccupMgmtProf', FloatType(), True),
+    StructField('MalePctDivorce', FloatType(), True),
+    StructField('MalePctNevMarr', FloatType(), True),
+    StructField('FemalePctDiv', FloatType(), True),
+    StructField('TotalPctDiv', FloatType(), True),
+    StructField('PersPerFam', FloatType(), True),
+    StructField('PctFam2Par', FloatType(), True),
+    StructField('PctKids2Par', FloatType(), True),
+    StructField('PctYoungKids2Par', FloatType(), True),
+    StructField('PctTeen2Par', FloatType(), True),
+    StructField('PctWorkMomYoungKids', FloatType(), True),
+    StructField('PctWorkMom', FloatType(), True),
+    StructField('NumIlleg', FloatType(), True),
+    StructField('PctIlleg', FloatType(), True),
+    StructField('NumImmig', FloatType(), True),
+    StructField('PctImmigRecent', FloatType(), True),
+    StructField('PctImmigRec5', FloatType(), True),
+    StructField('PctImmigRec8', FloatType(), True),
+    StructField('PctImmigRec10', FloatType(), True),
+    StructField('PctRecentImmig', FloatType(), True),
+    StructField('PctRecImmig5', FloatType(), True),
+    StructField('PctRecImmig8', FloatType(), True),
+    StructField('PctRecImmig10', FloatType(), True),
+    StructField('PctSpeakEnglOnly', FloatType(), True),
+    StructField('PctNotSpeakEnglWell', FloatType(), True),
+    StructField('PctLargHouseFam', FloatType(), True),
+    StructField('PctLargHouseOccup', FloatType(), True),
+    StructField('PersPerOccupHous', FloatType(), True),
+    StructField('PersPerOwnOccHous', FloatType(), True),
+    StructField('PersPerRentOccHous', FloatType(), True),
+    StructField('PctPersOwnOccup', FloatType(), True),
+    StructField('PctPersDenseHous', FloatType(), True),
+    StructField('PctHousLess3BR', FloatType(), True),
+    StructField('MedNumBR', FloatType(), True),
+    StructField('HousVacant', FloatType(), True),
+    StructField('PctHousOccup', FloatType(), True),
+    StructField('PctHousOwnOcc', FloatType(), True),
+    StructField('PctVacantBoarded', FloatType(), True),
+    StructField('PctVacMore6Mos', FloatType(), True),
+    StructField('MedYrHousBuilt', FloatType(), True),
+    StructField('PctHousNoPhone', FloatType(), True),
+    StructField('PctWOFullPlumb', FloatType(), True),
+    StructField('OwnOccLowQuart', FloatType(), True),
+    StructField('OwnOccMedVal', FloatType(), True),
+    StructField('OwnOccHiQuart', FloatType(), True),
+    StructField('RentLowQ', FloatType(), True),
+    StructField('RentMedian', FloatType(), True),
+    StructField('RentHighQ', FloatType(), True),
+    StructField('MedRent', FloatType(), True),
+    StructField('MedRentPctHousInc', FloatType(), True),
+    StructField('MedOwnCostPctInc', FloatType(), True),
+    StructField('MedOwnCostPctIncNoMtg', FloatType(), True),
+    StructField('NumInShelters', FloatType(), True),
+    StructField('NumStreet', FloatType(), True),
+    StructField('PctForeignBorn', FloatType(), True),
+    StructField('PctBornSameState', FloatType(), True),
+    StructField('PctSameHouse85', FloatType(), True),
+    StructField('PctSameCity85', FloatType(), True),
+    StructField('PctSameState85', FloatType(), True),
+    StructField('LemasSwornFT', FloatType(), True),
+    StructField('LemasSwFTPerPop', FloatType(), True),
+    StructField('LemasSwFTFieldOps', FloatType(), True),
+    StructField('LemasSwFTFieldPerPop', FloatType(), True),
+    StructField('LemasTotalReq', FloatType(), True),
+    StructField('LemasTotReqPerPop', FloatType(), True),
+    StructField('PolicReqPerOffic', FloatType(), True),
+    StructField('PolicPerPop', FloatType(), True),
+    StructField('RacialMatchCommPol', FloatType(), True),
+    StructField('PctPolicWhite', FloatType(), True),
+    StructField('PctPolicBlack', FloatType(), True),
+    StructField('PctPolicHisp', FloatType(), True),
+    StructField('PctPolicAsian', FloatType(), True),
+    StructField('PctPolicMinor', FloatType(), True),
+    StructField('OfficAssgnDrugUnits', FloatType(), True),
+    StructField('NumKindsDrugsSeiz', FloatType(), True),
+    StructField('PolicAveOTWorked', FloatType(), True),
+    StructField('LandArea', FloatType(), True),
+    StructField('PopDens', FloatType(), True),
+    StructField('PctUsePubTrans', FloatType(), True),
+    StructField('PolicCars', FloatType(), True),
+    StructField('PolicOperBudg', FloatType(), True),
+    StructField('LemasPctPolicOnPatr', FloatType(), True),
+    StructField('LemasGangUnitDeploy', FloatType(), True),
+    StructField('LemasPctOfficDrugUn', FloatType(), True),
+    StructField('PolicBudgPerPop', FloatType(), True),
+    StructField('ViolentCrimesPerPop', FloatType(), True),
+     ])
+
+
+
 ###  data cleaning ###
 
 def question_1_police_operating_budget (df):
@@ -40,23 +176,26 @@ def question_3_community_largest_population (df):
 	(df.where(F.col("population")!='?')
 	.groupBy('communityname')
 	.agg(
+		F.round(
 			F.sum(
-				F.col("population"))
+				F.col("population")),2)
 	.alias("Community_largest_population"))
 	.orderBy(F.col("Community_largest_population")
 	.desc())
 	.show())	
 
 def question_4_community_black_people (df):
-(df.where(F.col("racepctblack")!='?')
+	(df.where(F.col("racepctblack")!='?')
 	.groupBy('communityname')
 	.agg(
+		F.round(
 			F.sum(
-				F.col("racepctblack"))
+				F.col("racepctblack")),2)
 	.alias("Community_black_people"))
 	.orderBy(F.col("Community_black_people")
 	.desc())
-	.show())	
+	.show())
+	
 
 
 if __name__ == "__main__":
@@ -66,7 +205,7 @@ if __name__ == "__main__":
 	df = (spark.getOrCreate().read
 		          .format("csv")
 		          .option("header", "true")
-		          #.schema(schema_communities_crime)
+		          .schema(schema_communities_crime)
 		          .load("/home/spark/capgemini-aceleracao-pyspark-old/data/communities-crime/communities-crime.csv"))
 
 #df.orderBy('communityname').agg({'PolicBudgPerPop': 'max'}).show()
@@ -79,3 +218,4 @@ if __name__ == "__main__":
 question_1_police_operating_budget(df)
 question_2_violentcrimes_percommunity(df)
 question_3_community_largest_population(df)
+question_4_community_black_people(df)
