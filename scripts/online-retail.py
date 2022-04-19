71a865c5b3eb48db11fd692c24a64c4cd61fd0f1
Daniel Santos <dbispods@gmail.com>
2022-04-19 09:25:13 -0300

    updated code, now it's works

--- scripts/online-retail.py
+++ scripts/online-retail.py
@@ -1,15 +1,8 @@
 # importing libs needed for development #
 
-import re
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-import pandas as pd
-import pyspark.sql
-
-import numpy as np
-from pyspark.sql.functions import from_unixtime, unix_timestamp,expr
-from pyspark.sql.functions import isnan, when, count, col
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType, TimestampType
 
 ### analyzing data integrity ###
@@ -37,43 +30,42 @@ def check_empty_column(col):
 
 # checking the quality on specific columns #
 
-def col_quantity_qa(df):
-    df = df.withColumn('Quantity_qa', 
-                        F.when(F.col('Quantity').isNull(), 'M')) # M = Missing values #
-    df.groupBy('Quantity_qa').count().show()
+def qa_InvoiceNo(df):
+	df = df.withColumn("qa_InvoiceNo", 
+	F.when(F.col("InvoiceNo").startswith("C"), "C")
+	 .when(F.col("InvoiceNo").rlike("^[0-9]*$"), "OK").otherwise("F"))
+	(df.groupBy("qa_InvoiceNo").count().distinct().orderBy("qa_InvoiceNo", ascending=False).show())
 
-    return df
+def qa_StockCode(df):
+	df = df.withColumn("qa_StockCode", 
+	F.when(~F.col("StockCode").rlike("([0-9a-zA-Z]{5})"), "F")
+	 .otherwise("OK"))
+	(df.groupBy("qa_StockCode").count().distinct().orderBy("qa_StockCode", ascending=False).show())
 
-def col_invoicedate_qa(df):
-    df = df.withColumn('InvoiceDate_qa', F.when(check_is_empty('InvoiceDate'), 'M')) # M = Missing values #
-    
-    df = df.withColumn('Quantity_qa', 
-                    F.when(F.col('Quantity').isNull(), 'M')) # M = Missing values #
+def qa_Description(df):
+	df = df.withColumn("qa_Description", 
+	F.when(F.col("Description").isNull(), "M")
+	 .when(F.col("Description") == "", "M")
+	 .otherwise("OK"))
+	(df.groupBy("qa_Description").count().distinct().orderBy("qa_Description", ascending=False).show())
 
-    df.groupBy('Quantity_qa').count().show()
+def qa_Quantity(df):
+	df = df.withColumn("qa_Quantity", 
+	F.when(~F.col("Quantity").rlike("\d"), "N")
+	 .otherwise("OK"))
+	(df.groupBy("qa_Quantity").count().distinct().orderBy("qa_Quantity", ascending=False).show())		
 
-    df.groupBy('InvoiceDate_qa').count().show() 
+def qa_CustomerID(df):
+	df = df.withColumn("qa_CustomerID", 
+	F.when(~F.col("CustomerID").rlike("([0-9a-zA-Z]{5})"), "F").otherwise("OK"))
+	(df.groupBy("qa_CustomerID").count().distinct().orderBy("qa_CustomerID", ascending=False).show())
 
-    return df
-
-def col_unitprice_qa(df):
-    df = df.withColumn("UnitPrice_qa", 
-                    F.when(check_is_empty('UnitPrice'), 'M') # M = Missing values #
-                    .when(F.col('UnitPrice').contains(','), 'F')  # F = Float type values #
-                    .when(F.col('UnitPrice').rlike('[^0-9]'), 'A') # A = Number type values #
-    )
-    df = df.withColumn('Quantity_qa',
-                    F.when(F.col('Quantity').isNull(), 'M')) # M = Missing values #
-
-    df = df.withColumn('InvoiceDate_qa', F.when(check_is_empty('InvoiceDate'), 'M')) # M = Missing values #
-
-    df.groupBy('UnitPrice_qa').count().show()
-
-    df.groupBy('Quantity_qa').count().show()	
-
-    df.groupBy('InvoiceDate_qa').count().show() 
-
-    return df
+def qa_Country(df):
+	df = df.withColumn("qa_Country", 
+	F.when(F.col("Country").isNull(), "M")
+	 .when(F.col("Country") == "", "M")
+	 .otherwise("OK"))
+	(df.groupBy("qa_Country").count().distinct().orderBy("qa_Country", ascending=False).show())
 
 ### ---------------------------------------------- ###
 
@@ -123,183 +115,153 @@ def col_unitprice_tf(df):
 
 # answering some business questions #
 
-def question_1(df):
-	(df
-	.filter((F.col('StockCode').startswith('gift_0001')) & # Only gift cards starts with 'gift_001' #
-			(~F.col('InvoiceNo').startswith('C')) & # Disregarding canceled sales #
-			(F.col('Quantity') > 0)) # only completed sales #
-	.agg({'Quantity' : 'sum'})
-	.show())
+def pergunta_1(df):
+	df = df.withColumn('UnitPrice', F.regexp_replace(F.col('UnitPrice'), ',', '.').cast('float'))
+	(df.where(F.col('StockCode').rlike('gift_0001'))
+			.agg(F.round(F.sum(F.col('UnitPrice') * F.col('Quantity')), 2).alias('total_gift_cards')).show())	
 
-def question_2(df):
-	
-	(df
-	.filter((F.col('StockCode').startswith('gift_0001')) & # Only gift cards starts with 'gift_001' #
-			(~F.col('InvoiceNo').startswith('C')) & # Disregarding canceled sales #
-			(F.col('Quantity') > 0)) # only completed sales #
-	.groupBy(F.month('InvoiceDate'))
-	.sum('Quantity')
-	.orderBy('month(InvoiceDate)')
-	.show())
+def pergunta_2(df):
+	df = (df.withColumn('UnitPrice', F.regexp_replace(F.col('UnitPrice'), ',', '.').cast('float'))
+			.withColumn("InvoiceDate", F.to_timestamp(F.col("InvoiceDate"), "d/M/yyyy H:m")))
+	(df.where(F.col('StockCode').rlike('gift_0001').alias('Gift_Cards'))	
+			.groupBy(F.month("InvoiceDate").alias('mes'))
+			.agg(F.round(F.sum('UnitPrice'), 2).alias('total_gift_Cards_month'))
+			.orderBy('mes').show())
 
-def question_3(df):
+def pergunta_3(df):
 
-	(df
-	.filter((F.col('StockCode') == 'S') & # Only samples #
-			(~F.col('InvoiceNo').startswith('C')) & # Disregarding canceled sales #
-			(F.col('Quantity') > 0))  # only completed sales #
-	.groupBy('StockCode').sum('Quantity')
-	.show())
-	
-def question_4(df):
+	df = (df.withColumn('UnitPrice', F.regexp_replace(F.col('UnitPrice'), ',', '.').cast('float'))
+			.withColumn("UnitPrice", F.when(F.col("InvoiceNo").startswith('C'), 0).otherwise(F.col("UnitPrice")))
+			)
+	(df.where(F.col('StockCode')== 'S')
+			.agg(F.round(F.sum(F.col('UnitPrice')), 2).alias('total_samples')).show())	
 
-	(df
-	.filter((~F.col('InvoiceNo').startswith('C')) & # Disregarding canceled sales #
-			(F.col('Quantity') > 0) &  # only completed sales #
-			(F.col('StockCode') != 'PADS')) # Disregarding PADS code #
-	.groupBy('StockCode')
-	.sum('Quantity')
-	.orderBy(F.col('sum(Quantity)').desc())
-	.show())
+def pergunta_4(df):
+	df = (df.withColumn("Quantity", F.when(F.col("Quantity").isNull() | (F.col("Quantity") < 0), 0)
+			.otherwise(F.col("Quantity")))
+			)
+	(df.where(~F.col('StockCode').rlike('C'))
+			.groupBy(F.col('Description'))
+			.agg(F.sum('Quantity').alias('Quantity'))
+			.orderBy(F.col('Quantity').desc())
+			.limit(1)
+			.show())
+			
+def pergunta_5(df):
+	df = (df.withColumn("Quantity", F.when(F.col("Quantity").isNull() | (F.col("Quantity") < 0), 0)
+			.otherwise(F.col("Quantity")))
+			.withColumn("InvoiceDate", F.to_timestamp(F.col("InvoiceDate"), "d/M/yyyy H:m"))
+			)	
+	(df.where(~F.col('StockCode').rlike('C'))
+			.groupBy('Description', F.month('InvoiceDate').alias('month'))
+			.agg(F.sum('Quantity').alias('Quantity'))
+			.orderBy(F.col('Quantity').desc()).dropDuplicates(['month'])
+			.show())
 
+def pergunta_6(df):
+	df = (df.withColumn("Quantity", F.when(F.col("Quantity").isNull() | (F.col("Quantity") < 0), 0)
+			.otherwise(F.col("Quantity")))
+			.withColumn("InvoiceDate", F.to_timestamp(F.col("InvoiceDate"), "d/M/yyyy H:m"))
+			.withColumn('UnitPrice', F.regexp_replace(F.col('UnitPrice'), ',', '.').cast('float'))
+			.withColumn('UnitPrice', F.when(F.col('UnitPrice').isNull() | (F.col('UnitPrice') < 0), 0)
+			.otherwise(F.col('UnitPrice')))
+			)
+	(df.where(~F.col('StockCode').rlike('C'))
+			.groupBy(F.hour('InvoiceDate').alias('hour_sales'))
+			.agg(F.round(F.sum(F.col('UnitPrice') * F.col('Quantity')), 2).alias('valor'))
+			.orderBy(F.col('valor').desc())
+			.limit(1)
+			.show())
 
-def question_5(df):
+def pergunta_7(df):
+	df = (df.withColumn("Quantity", F.when(F.col("Quantity").isNull() | (F.col("Quantity") < 0), 0)
+			.otherwise(F.col("Quantity")))
+			.withColumn("InvoiceDate", F.to_timestamp(F.col("InvoiceDate"), "d/M/yyyy H:m"))
+			.withColumn('UnitPrice', F.regexp_replace(F.col('UnitPrice'), ',', '.').cast('float'))
+			.withColumn('UnitPrice', F.when(F.col('UnitPrice').isNull() | (F.col('UnitPrice') < 0), 0)
+			.otherwise(F.col('UnitPrice')))
+			)
+	(df.where(~F.col('StockCode').rlike('C'))
+			.groupBy(F.month('InvoiceDate').alias('month_sales'))
+			.agg(F.round(F.sum(F.col('UnitPrice') * F.col('Quantity')), 2).alias('valor'))
+			.orderBy(F.col('valor').desc())
+			.limit(1)
+			.show())
 
-	df = (
-		df
-		.filter((~F.col('InvoiceNo').startswith('C')) & # Disregarding canceled sales #
-				(F.col('Quantity') > 0) &  # only completed sales #
-				(F.col('StockCode') != 'PADS')) # Disregarding PADS code #
-		.groupBy('StockCode', F.month('InvoiceDate'))
-		.sum('Quantity')
-		.orderBy(F.col('sum(Quantity)').desc())
-	)
+def pergunta_8(df):
+	df = (df.withColumn("Quantity", F.when(F.col("Quantity").isNull() | (F.col("Quantity") < 0), 0)
+			.otherwise(F.col("Quantity")))
+			.withColumn("InvoiceDate", F.to_timestamp(F.col("InvoiceDate"), "d/M/yyyy H:m"))
+			.withColumn('UnitPrice', F.regexp_replace(F.col('UnitPrice'), ',', '.').cast('float'))
+			.withColumn('UnitPrice', F.when(F.col('UnitPrice').isNull() | (F.col('UnitPrice') < 0), 0)
+			.otherwise(F.col('UnitPrice')))
+			)
+	(df.where(~F.col('StockCode').rlike('C'))
+			.groupBy('Description', F.year('InvoiceDate').alias('year'), F.month('InvoiceDate').alias('month'))
+			.agg(F.round(F.sum(F.col('UnitPrice') * F.col('Quantity')), 2).alias('valor'))
+			.orderBy(F.col('valor').desc()).dropDuplicates(['month'])
+			.show())
 
+def pergunta_9(df):
+	df = (df.withColumn("Quantity", F.when(F.col("Quantity").isNull() | (F.col("Quantity") < 0), 0)
+			.otherwise(F.col("Quantity")))
+			.withColumn('UnitPrice', F.regexp_replace(F.col('UnitPrice'), ',', '.').cast('float'))
+			.withColumn('UnitPrice', F.when(F.col('UnitPrice').isNull() | (F.col('UnitPrice') < 0), 0)
+			.otherwise(F.col('UnitPrice')))
+			)
+	(df.where(~F.col('StockCode').rlike('C'))
+			.groupBy(F.col('Country').alias('country_sales'))
+			.agg(F.round(F.sum(F.col('UnitPrice') * F.col('Quantity')), 2).alias('valor'))
+			.orderBy(F.col('valor').desc())
+			.limit(1)
+			.show())
 
-	df = df.select('StockCode',
-				F.col('month(InvoiceDate)').alias('month'),
-				F.col('sum(Quantity)').alias('sum_quantity'))
-	
-	df_max_per_month = df.groupBy('month').max('sum_quantity')
+def pergunta_10(df):
+	df = (df.withColumn("Quantity", F.when(F.col("Quantity").isNull() | (F.col("Quantity") < 0), 0)
+			.otherwise(F.col("Quantity")))
+			.withColumn('UnitPrice', F.regexp_replace(F.col('UnitPrice'), ',', '.').cast('float'))
+			.withColumn('UnitPrice', F.when(F.col('UnitPrice').isNull() | (F.col('UnitPrice') < 0), 0)
+			.otherwise(F.col('UnitPrice')))
+			)	
+	(df.where(F.col('StockCode')=='M')
+			.groupBy(F.col('Country').alias('manual_country_sales'))
+			.agg(F.round(F.sum(F.col('UnitPrice') * F.col('Quantity')), 2).alias('valor'))
+			.orderBy(F.col('valor').desc())
+			.limit(1)
+			.show())						
 
-	df_max_per_month = df_max_per_month.join(df.alias('b'), 
-								F.col('b.sum_quantity') == F.col('max(sum_quantity)'),
-								"left").select('b.month','StockCode','sum_quantity')
-	
-	df_max_per_month.orderBy('month').show()
+def pergunta_11(df):
+	df = (df.withColumn("Quantity", F.when(F.col("Quantity").isNull() | (F.col("Quantity") < 0), 0)
+			.otherwise(F.col("Quantity")))
+			.withColumn('UnitPrice', F.regexp_replace(F.col('UnitPrice'), ',', '.').cast('float'))
+			.withColumn('UnitPrice', F.when(F.col('UnitPrice').isNull() | (F.col('UnitPrice') < 0), 0)
+			.otherwise(F.col('UnitPrice')))
+			)
+	(df.where(~F.col('StockCode').rlike('C'))
+			.groupBy(F.col('InvoiceNo').alias('nf_sales'))
+			.agg(F.round(F.sum(F.col('UnitPrice') * F.col('Quantity')), 2).alias('valor'))
+			.orderBy(F.col('valor').desc())
+			.limit(1)
+			.show())
 
+def pergunta_12(df):
+	df = (df.withColumn("Quantity", F.when(F.col("Quantity").isNull() | (F.col("Quantity") < 0), 0)
+			.otherwise(F.col("Quantity")))
+			)
+	(df.where(~F.col('StockCode').rlike('C'))
+			.groupBy(F.col('InvoiceNo').alias('nf_itens'))
+			.agg(F.sum(F.col('Quantity')).alias('total'))
+			.orderBy(F.col('total').desc())
+			.limit(1)
+			.show())
 
-def question_6(df):
-	
-	(df
-	.filter(F.col('valor_de_venda') > 0 &  # only completed sales #
-	 		(F.col('StockCode') != 'PADS')) # Disregarding PADS code #
-	.groupBy(F.hour('InvoiceDate'))
-	.sum('valor_de_venda')
-	.orderBy(F.col('sum(valor_de_venda)').desc())
-	.show())
-
-
-
-
-def question_7(df):
-	
-	(df
-	.filter(F.col('valor_de_venda') > 0 &   # only completed sales #
-			(F.col('StockCode') != 'PADS')) # Disregarding PADS code #
-	.groupBy(F.month('InvoiceDate'))
-	.sum('valor_de_venda')
-	.orderBy(F.col('sum(valor_de_venda)').desc())
-	.show())
-
-
-
-
-def question_8(df):
-
-	(df
-	.filter((F.col('valor_de_venda') > 0) &  # only completed sales #
-			(F.col('StockCode') != 'PADS')) # Disregarding PADS code #
-	.groupBy(F.month('InvoiceDate'), F.col('StockCode'))
-	.sum('valor_de_venda')
-	.orderBy(F.col('sum(valor_de_venda)').desc())
-	.show(1))
-
-
-
-def question_9(df):
-
-	(df
-	.filter((F.col('valor_de_venda') > 0) & # only completed sales #
-			(F.col('StockCode') != 'PADS')) # Disregarding PADS code #
-	.groupBy('Country')
-	.sum('valor_de_venda')
-	.orderBy(F.col('sum(valor_de_venda)').desc())
-	.show(1))
-
-
-
-def question_10(df):
-		
-	(df
-	.filter((F.col('valor_de_venda') > 0) & # only completed sales #
-			(F.col('StockCode') != 'PADS') & # Disregarding PADS code #
-			(F.col('StockCode') == 'M')) # Only manual sales #
-	.groupBy('Country')
-	.sum('valor_de_venda')
-	.orderBy(F.col('sum(valor_de_venda)').desc())
-	.show(1))
-
-
-
-
-
-def question_11(df):
-
-	maior_valor_de_venda = (df
-				.filter(F.col('StockCode') != 'PADS') # Disregarding PADS code #
-				.agg({'valor_de_venda' : 'max'})
-				.first()[0])
-
-	(df
-	.select('InvoiceNo','valor_de_venda')
-	.filter(F.col('valor_de_venda') == maior_valor_de_venda)
-	.show())
-
-
-def question_12(df):
-
-	maior_n_de_itens = (df
-				.filter(F.col('StockCode') != 'PADS') # Disregarding PADS code #
-				.agg({'Quantity' : 'max'})
-				.first()[0])
-
-	(df
-	.select('InvoiceNo','Quantity')
-	.filter(F.col('Quantity') == maior_n_de_itens)
-	.show())
-
-def question_13(df):
-
-	mais_frequente = (df
-					.filter(
-						(F.col('StockCode') != 'PADS') & # Disregarding PADS code #
-						(F.col('CustomerID').isNotNull()) #  Disregarding empty info from CustomerID #
-						)
-					.groupBy('CustomerID')
-					.count()
-					.orderBy(F.col('count').desc())
-					.first()[1])
-
-	(df
-	.filter(
-		(F.col('StockCode') != 'PADS') & # Disregarding PADS code #
-		(F.col('CustomerID').isNotNull()) #  Disregarding empty info from CustomerID #
-		)
-	.groupBy('CustomerID')
-	.count()
-	.filter(F.col('count') == mais_frequente)
-	.show())
+def pergunta_13(df):
+	(df.where(F.col("CustomerID").isNotNull())
+			.groupBy(F.col('CustomerID').alias('customer'))
+			.count()
+			.orderBy(F.col('count').desc())
+			.limit(1)
+			.show())	
 
 ### ---------------------------------------------- ###
 
@@ -331,58 +293,26 @@ if __name__ == "__main__":
 
 # calling previous functions #
 
-#df = col_quantity_qa(df)
-#df = col_quantity_tf(df)
-#question_1(df)
+	#qa_InvoiceNo(df)
+	#qa_StockCode(df)
+	#qa_Description(df)
+	#qa_Quantity(df)
+	#qa_CustomerID(df)
+	#qa_Country(df)
 
-#df = col_quantity_qa(df)
-#df = pergunta_2_tr(df)
-#pergunta_2(df)
-
-#df = pergunta_3_qa(df)
-#df = pergunta_3_tr(df)
-#pergunta_3(df)
-
-#df = pergunta_4_qa(df)
-#df = pergunta_4_tr(df)
-#pergunta_4(df)
-
-#df = pergunta_5_qa(df)
-#df = pergunta_5_tr(df)
-#pergunta_5(df)
-
-#df = pergunta_6_qa(df)
-#df = pergunta_6_tr(df)
-#pergunta_6(df)
-
-#df = pergunta_7_qa(df)
-#df = pergunta_7_tr(df)
-#pergunta_7(df)
-
-#df = pergunta_8_qa(df)
-#df = pergunta_8_tr(df)
-#pergunta_8(df)
-
-#df = pergunta_9_qa(df)
-#df = pergunta_9_tr(df)
-#pergunta_9(df)
-
-#df = pergunta_10_qa(df)
-#df = pergunta_10_tr(df)
-#pergunta_10(df)
-
-#df = pergunta_11_qa(df)
-#df = pergunta_11_tr(df)
-#pergunta_11(df)
-
-#df = pergunta_12_qa(df)
-#df = pergunta_12_tr(df)
-#pergunta_12(df)
-
-#pergunta_13_qa(df)
-#pergunta_13(df)
-
-
+	#pergunta_1(df)
+	#pergunta_2(df)
+	#pergunta_3(df)
+	#pergunta_4(df)
+	#pergunta_5(df)
+	#pergunta_6(df)
+	#pergunta_7(df)
+	#pergunta_8(df)
+	#pergunta_9(df)
+	#pergunta_10(df)
+	#pergunta_11(df)
+	#pergunta_12(df)
+	#pergunta_13(df)
 
 
 
